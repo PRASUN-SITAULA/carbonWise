e114ce43048faf1b1b75b28086b9d499d88ede4d
PRASUN-SITAULA <psitaula209@gmail.com>
2024-01-30 09:21:58 +0545

    basic setup of langchain

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const helmet = require('helmet')
+// const rateLimit  = require('express-rate-limit') //yet to be used
+
 
 const app = express()
 
+// security packages
+app.use(helmet())
 
 module.exports = app
 
