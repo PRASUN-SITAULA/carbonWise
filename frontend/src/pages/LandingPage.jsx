bac819a919b62b1199fc68c20b93913e87d33ade
Subash-Lamichhane <077bct081.subash@pcampus.edu.np>
2024-02-02 22:40:00 +0545

    some changes to design and added information card in formPage

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -5,11 +5,11 @@ import { Link } from 'react-router-dom'
 const LandingPage = () => {
     return (
         // from-lime-800 via-lime-600 to-lime-300
-        <div className='h-screen w-full flex flex-col gap-14 relative bg-gradient-to-r from-orange-300 from-[-25%] via-slate-600 via-15% to-gray-800 to-80%'
+        <div className='h-screen w-full flex flex-col gap-14 relative bg-gradient-to-t from-cyan-400 to-purple-800'
             style={{ clipPath: "polygon(0 0, 100% 0, 100% 70%, 0 100%)", }}>
             {/* backgroundImage: `url(${background_image})`, backgroundSize: 'cover', backgroundPosition: 'center'  */}
             <div className="header flex justify-center">
-                <header className='w-[80%] h-14 flex justify-between text-white mt-20 items-center mx-5'>
+                <header className='w-full md:w-[80%] h-14 flex justify-between text-white mt-20 items-center mx-5'>
                     <Link to='/'><div className="text-3xl">
                         Carbon Footprint Advisor
                     </div>
@@ -28,7 +28,7 @@ const LandingPage = () => {
             </div>
             <div className="w-full flex justify-center items-center">
 
-                <div className="main grid place-items-center grid-cols-3 w-[80%] mt-10">
+                <div className="main grid place-items-center grid-cols-1 md:grid-cols-3 w-[80%] mt-14">
                     <div>
                         {/* <h5 className="text-5xl bold text-white font-bold">
                             Daily Diary  <span className='font-thin'>&</span>
