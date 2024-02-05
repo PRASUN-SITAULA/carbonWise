5503666d4ddff6bcc798ca8842db56a9dd980ce4
PRASUN-SITAULA <psitaula209@gmail.com>
2024-02-05 17:46:32 +0545

    changed input to LLM

--- backend/Controller/llmController.js
+++ backend/Controller/llmController.js
@@ -16,7 +16,7 @@ exports.getAnswer = async (req, res) =>
     const outputParser = new StringOutputParser()
 
     const prompt = ChatPromptTemplate.fromMessages([
-        ["system", "You are a world class professor of environmental science and carbon footprint."],
+        ["system", "You are a world class professor of environmental science and environmental activist."],
         ["user", "{input}"],
     ])
 
@@ -26,8 +26,9 @@ exports.getAnswer = async (req, res) =>
 
     const { household, lifestyleData, transportData } = userData
 
-    const householdStr = `The household consists of ${household.numberOfPeople} with housing type ${household.housingType}. The total electricity consumption in a month is ${household.electricityConsumption} 
-    with clean energy percentage ${household.cleanEnergyPercentage} and heating source is ${household.heatingEnergySource}.`
+    // household string to be given to LLM.
+    const householdStr = `The household consists of ${household.numberOfPeople} with housing type ${household.housingType}. The total electricity consumption in KWh in a month is ${household.electricityConsumption} 
+    with clean energy percentage ${household.cleanEnergyPercentage} and heating source as ${household.heatingEnergySource}.`
     
     // take the object of the materials that the user recycles and convert it to string.
     const wasteHandling = lifestyleData.wasteHandling
@@ -35,31 +36,35 @@ exports.getAnswer = async (req, res) =>
     const recyclableMaterialsStr = recyclableMaterials.join(", ");
 
     const lifestyleStr = `The household preffered diet is ${lifestyleData.preferredDiet}. The answer to do you buy mostly local products is ${lifestyleData.buyLocalProducts}. 
-    The answer to Do you buy from environmentally responsible companies? is ${lifestyleData.buyFromEnvironmentallyResponsible}. The family eats out ${lifestyleData.eatOutFrequency} times a week. 
+    The answer to Do you buy from environmentally responsible companies is ${lifestyleData.buyFromEnvironmentallyResponsible}. The family eats out ${lifestyleData.eatOutFrequency} times a week. 
     The individual recyles ${recyclableMaterialsStr}.`
 
-    // const transportationStr = `The family may travel in train, bus or tram. The average hours travelled in respective vehicles are ${transportData.intercityTrain}, 
-    // ${transportData.subway}, ${transportData.intercityBus}, ${transportData.cityBus}, ${transportData.tram} and ${transportData.bikewalk}. `
+    // transportation string
+    const transportationStr = `The family may travel in train, bus or tram. The average distance travelled in respective vehicles are ${transportData.Train}
+    ${transportData.Bus}, ${transportData.Tram}.`
     
-        // Filter out empty/unused modes of transport
-    const usedTransport = Object.entries(transportData)
-    .filter(([key, value]) => value !== 0 && key !== 'useCar' && key !== 'useMotorbike' && key !== 'carMileage' && key !== 'carConsumption' && key !== 'motorbikeMileage' && key !== 'motorbikeConsumption')
-    .map(([key, value]) => `${key}: ${value}`);
-
-    // Add private flights information if used
-    if (transportData.privateFlights.longRange > 0 || transportData.privateFlights.shortRange > 0) {
-    usedTransport.push(`privateFlights: ${transportData.privateFlights.longRange}+${transportData.privateFlights.shortRange}`);
+    if(transportData.useCar == 'Yes'){
+        transportationStr += `The family also owns a car and they travel a total distance of ${transportData.carDistanceTraveled}`
+    }
+    else{
+        transportationStr += `The family do not use a car.`
     }
 
-    // Join modes of transport with commas
-    const transportationStr = usedTransport.join(", ");
+    if(transportData.useMotorbike == 'Yes'){
+        transportationStr += `The family also owns a motor bike  and they travel a total distance of ${transportData.motorbikeDistanceTraveled}`
+    }
+    else{
+        transportationStr += `The family do not use a motorbike.`
+    }
 
+    transportationStr += `The total distance travelled by the family in short range flight is ${transportData.privateFlights["shortRange"]} and in long range flight is ${transportData.privateFlights["longRange"]}`
 
     const answer = await chain.invoke({
-        input: `Give ways to reduce carbon footprint based on following data of an individual's household, lifestyle and transportation: 
+        input: `Give ways to reduce carbon footprint based on following data of family's household, lifestyle and transportation: 
         ${householdStr}\n
         ${lifestyleStr}\n
         ${transportationStr}
+        Give the answer by grouping household, lifestyle and transportation.
         `
     })
     const [electricityCarbonEmission, totalWasteCarbonEmission, transporationCarbonEmission] = calculateCarbonFootprint(userData)
